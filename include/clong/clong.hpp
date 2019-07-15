70f525ad482b52844536b4c8e60db9584a15597f
Charly Chevalier <charlyy.chevalier@gmail.com>
2019-07-16 00:00:07 +0200

    Add fmt third party package

--- include/clong/clong.hpp
+++ include/clong/clong.hpp
@@ -2,6 +2,7 @@
 #define CLONG_CLONG_HPP
 
 #include <clong/config.hpp>
+#include <clong/format.hpp>
 #include <clong/Context.hpp>
 #include <clong/Visitor.hpp>
 #include <clong/jekyll/JustTheDocs.hpp>
---
+++ include/clong/format.hpp
@@ -0,0 +1,14 @@
+#ifndef CLONG_FMT_HPP
+#define CLONG_FMT_HPP
+
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+#undef  FMT_HEADER_ONLY
+
+namespace clong {
+
+using namespace ::fmt;
+
+}
+
+#endif
