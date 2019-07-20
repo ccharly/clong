cd9b1ed265ae2949a65bc678be3a3615b5e93c7d
Charly Chevalier <charlyy.chevalier@gmail.com>
2019-07-20 15:52:22 +0200

    travis: Try to limit MSVC warnings from clang

--- include/clong/config.hpp
+++ include/clong/config.hpp
@@ -1,6 +1,16 @@
 #ifndef CLONG_CONFIG_HPP
 #define CLONG_CONFIG_HPP
 
+/// Current version
 #define CLONG_VERSION "0.0.0"
+
+/// True if compiling using MSVC
+#define CLONG_IS_MSVC _MSC_VER && !__INTEL_COMPILER
+
+/// True if compiling using Clang
+#define CLONG_IS_CLANG defined(__clang__)
+//
+/// True if compiling using GCC
+#define CLONG_IS_CLANG defined(__GNUC__) || defined(__GNUG__)
 
 #endif
